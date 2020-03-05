cefaf71621ed6091f59449baf2bbb9d29aea4b07
Filippo Mailli <filippo.mailli@gmail.com>
2020-03-05 23:30:01 +0100

    .5 Invio messaggio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
+    $('#invia-messaggio').click(function() {
+        var messaggioInput = $('#mex-bar').val();
+        $('#mex-bar').val('');
+        var messaggio = $('.template .messaggio-inviato').clone();
+        messaggio.children('.testo-messaggio').text(messaggioInput);
+        messaggio.children('#time-mex').text(x);
+        $('.corpo-chat').append(messaggio);
 
-    
+    });
+
+    var d = new Date();
+    var x = d.getHours() + "." + d.getMinutes();
 
 });
