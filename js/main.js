aa66512f1fdc23bff2e1a92d92512825198bc7c4
Filippo Mailli <filippo.mailli@gmail.com>
2020-03-06 01:06:45 +0100

    .6 Risposta + barra di ricerca

--- js/main.js
+++ js/main.js
@@ -6,10 +6,32 @@ $(document).ready(function() {
         messaggio.children('.testo-messaggio').text(messaggioInput);
         messaggio.children('#time-mex').text(x);
         $('.corpo-chat').append(messaggio);
+        setTimeout(mexOuput, 1000);
+    });
 
+
+    function mexOuput() {
+        var messaggioOut = $('.template-ricevuto .messaggio-ricevuto').clone();
+        messaggioOut.children('.testo-ricevuto');
+        messaggioOut.children('#time-mex').text(x);
+        $('.corpo-chat').append(messaggioOut);
+    };
+
+    $('#search-bar').keyup(function(event) {
+        var carattereFiltro = $(this).val().toLowerCase();
+        $('.testo-amico h3').each(function() {
+            if ($(this).text().toLowerCase().includes(carattereFiltro)) {
+                $(this).closest(".profilo-amico").show();
+            } else {
+                $(this).closest(".profilo-amico").hide();
+            }
+        });
     });
 
     var d = new Date();
-    var x = d.getHours() + "." + d.getMinutes();
-
+    var minutes = d.getMinutes();
+    var hours = d.getHours();
+    var x = hours + "." + minutes;
+    if (minutes < 10){
+        minutes = "0" + minutes; }
 });
