d76059df8731c23204eacdd058ac1bafb70f1869
Filippo Mailli <filippo.mailli@gmail.com>
2020-03-10 14:31:23 +0100

    .7 Modifiche milestone 2

--- js/main.js
+++ js/main.js
@@ -1,37 +1,84 @@
 $(document).ready(function() {
+//     $('#invia-messaggio').click(function() {
+//         var messaggioInput = $('#mex-bar').val();
+//         $('#mex-bar').val('');
+//         var messaggio = $('.template .messaggio-inviato').clone();
+//         messaggio.children('.testo-messaggio').text(messaggioInput);
+//         messaggio.children('#time-mex').text(x);
+//         $('.corpo-chat').append(messaggio);
+//         setTimeout(mexOuput, 1000);
+//     });
+
+    $('#mex-bar').focus(function() {
+        $('#microfono-mex').toggleClass('no-active');
+        $('#invia-messaggio').toggleClass('no-active');
+    }).blur(function () {
+        $('#microfono-mex').toggleClass('no-active');
+        $('#invia-messaggio').toggleClass('no-active');
+    });
+
     $('#invia-messaggio').click(function() {
+        invioMessaggio();
+    });
+
+    $('#mex-bar').keypress(function(event) {
+        if(event.keyCode == 13) {
+            invioMessaggio();
+        }
+    });
+
+    function invioMessaggio() {
         var messaggioInput = $('#mex-bar').val();
-        $('#mex-bar').val('');
-        var messaggio = $('.template .messaggio-inviato').clone();
-        messaggio.children('.testo-messaggio').text(messaggioInput);
-        messaggio.children('#time-mex').text(x);
-        $('.corpo-chat').append(messaggio);
-        setTimeout(mexOuput, 1000);
-    });
+        if(messaggioInput.trim().length > 0) {
+            $('#mex-bar').val('');
+            creaMessaggio(messaggioInput, 'messaggio-inviato');
+            scroll();
+            setTimeout(function() {
+                creaMessaggio('ok', 'messaggio-ricevuto');
+                scroll();
+            }, 1000);
+        }
+    }
 
+    function creaMessaggio(testoMessaggio, inviatoRicevuto) {
+        var templateMsg = $('.template .messaggio').clone();
+        templateMsg.children('.testo-messaggio').text(testoMessaggio);
+        templateMsg.children('#time-mex').text(x);
+        templateMsg.addClass(inviatoRicevuto);
+        $('.corpo-chat').append(templateMsg);
+    }
 
-    function mexOuput() {
-        var messaggioOut = $('.template-ricevuto .messaggio-ricevuto').clone();
-        messaggioOut.children('.testo-ricevuto');
-        messaggioOut.children('#time-mex').text(x);
-        $('.corpo-chat').append(messaggioOut);
-    };
+    function scroll() {
+        var pixelScroll = $('.corpo-chat').height();
+        $('.corpo-chat').scrollTop(pixelScroll);
+    }
 
-    $('#search-bar').keyup(function(event) {
-        var carattereFiltro = $(this).val().toLowerCase();
-        $('.testo-amico h3').each(function() {
-            if ($(this).text().toLowerCase().includes(carattereFiltro)) {
-                $(this).closest(".profilo-amico").show();
-            } else {
-                $(this).closest(".profilo-amico").hide();
-            }
-        });
-    });
-
+//
+//
+//     function mexOuput() {
+//         var messaggioOut = $('.template-ricevuto .messaggio-ricevuto').clone();
+//         messaggioOut.children('.testo-ricevuto');
+//         messaggioOut.children('#time-mex').text(x);
+//         $('.corpo-chat').append(messaggioOut);
+//     };
+//
+//     $('#search-bar').keyup(function(event) {
+//         var carattereFiltro = $(this).val().toLowerCase();
+//         $('.testo-amico h3').each(function() {
+//             if ($(this).text().toLowerCase().includes(carattereFiltro)) {
+//                 $(this).closest(".profilo-amico").show();
+//             } else {
+//                 $(this).closest(".profilo-amico").hide();
+//             }
+//         });
+//     });
+//
     var d = new Date();
     var minutes = d.getMinutes();
     var hours = d.getHours();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
     var x = hours + "." + minutes;
-    if (minutes < 10){
-        minutes = "0" + minutes; }
+
 });
